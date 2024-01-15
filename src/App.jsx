a28b8c7822929668d335888d95639eae452dd48b
Nathan George <georgenathan010@gmail.com>
2024-01-15 12:58:03 +0300

    added a server and called the data. Everything works correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useFetch from './utils/useFetch'
 
 const App = () => {
+
+  const [url, setUrl] = useState('http://localhost:3000/Students');
+
+  const {data, isPending, error} = useFetch(url);
+  console.log(data[0].name)
+
   return (
-    <div>App</div>
+    <div>
+      {isPending && <h1>Loading...</h1> }
+      {error && <h1>{`${error}`}</h1>}
+      {data && data.map((item)=> {
+        <h1> {item.name}</h1>
+      })}
+    </div>
   )
 }
 
